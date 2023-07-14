9fa71a7d7b6448feb11fa490ee7bd06f5b1c4bb8
lindholmadam <adam-lindholm@hotmail.com>
2023-07-14 12:24:29 +0200

    frontend removed from remote

--- frontend/src/constants.js
+++
@@ -1,4 +0,0 @@
-// export const BASE_URL =
-// process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const BASE_URL = ''; // If using proxy
-export const USERS_URL = '/api/users';
